178ffd40a78a5a02b1e5587cf0b2a131611734db
vdurbha <viswanath.durbha@gmail.com>
2020-06-26 20:56:08 +0530

    Adding Google OR Framework

--- lib/java_buildpack/framework/google_or_framework.rb
+++ lib/java_buildpack/framework/google_or_framework.rb
@@ -13,33 +13,27 @@ module JavaBuildpack
       def initialize(context)
         super(context)
         @logger = JavaBuildpack::Logging::LoggerFactory.instance.get_logger GoogleORFramework
-        @logger.info { "Inside initialize method of GoogleORFramework" }
       end
 
       # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
         #download_tar(false, @droplet.sandbox, 'Google OR Framework')
-        @logger.info { "Inside compile method of GoogleORFramework" }
         download_tar
         @droplet.copy_resources
       end
 
       # (see JavaBuildpack::Component::BaseComponent#release)
-      def release
-        @logger.info { "Inside release method of GoogleORFramework" }
-        @droplet.environment_variables
+      def release@droplet.environment_variables
           .add_environment_variable('LD_LIBRARY_PATH',
                                     "$LD_LIBRARY_PATH:#{qualify_path(lib, @droplet.root)}")
           .add_environment_variable('GOOGLE_OR_HOME', @droplet.sandbox)
       end
 
       def lib
-        @logger.info { "Inside lib method of GoogleORFramework" }        
         @droplet.sandbox + 'lib'
       end
 
       def supports?
-        @logger.info { "Inside supports method of GoogleORFramework" }        
         return true
       end
 
